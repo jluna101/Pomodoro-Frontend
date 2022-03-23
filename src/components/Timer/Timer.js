d66168fdfd343c0887227cdfa7eaf3e4acc51dcc
pspyoung <pspyoung2@gmail.com>
2022-03-23 11:35:06 -0700

    Fixes timer to start on click. Adds toggle functionality to start/pause timer, and swap icons. Removes comments

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,62 +1,65 @@
 // Timer.js
 
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Timer.css';
 
 function Timer(props) {
-	// create function for timer
-	// set initial time value (30s) counting down
-	// set timer interval to 1000 or 1s
 	// when timer reaches 0, stop timer
 	// render timer data on screen with useState
 
-	// add event handler on click to start timer
-	// add event handler on click to pause timer
-	// add event handler on click to reset timer
-
 	const [timer, setTimer] = useState(30);
+	const [isActive, setIsActive] = useState(false);
 
-	function pomTimer() {
-		let time = 30;
-		let timeInterval;
-		timeInterval = setInterval(pomTimer, 1000);
-		time--;
-		console.log('Timer works');
+	function toggleTimer() {
+		setIsActive(!isActive);
 	}
-	// set initial state of the timer to be 10 * 1000
-	// timeinterval: state that is changed every second.
-	// useEffect to change state every time a second passes
 
-	// useEffect(() => {}, []); // setTime(timeInterval);
-
-	// trial #1
-	// setTimer in pomTimer.
-	// inside pomTimer, set logic so that timer never goes below 0.
-	// call pomTimer onClick
-
-	// trial #2
-	// inside pomTimer, set logic so that timer never goes below 0.
-	// start timer and stop timer functions
-	// above function will either call pomTimer or it will clear interval that was set.
-	// const interval = setInterval(() => {
+	// function pomTimer() {
 	// 	setTimer((timer) => timer - 1);
-	// }, 1000);
+	// 	console.log('Timer works');
+	// }
 
-	// 		return () => clearInterval(interval);/ }, [time])
-	// , []time
+	// function startTimer() {
+	// 	clockInterval = setInterval(pomTimer, 1000);
+	// }
+
+	// create separate pause and reset functions
+	// add event listeners to buttons that runs those functions
+
+	useEffect(() => {
+		let clockInterval;
+		if (isActive) {
+			clockInterval = setInterval(() => {
+				setTimer((timer) => timer - 1);
+			}, 1000);
+		} else if (!isActive && timer !== 0) {
+			clearInterval(clockInterval);
+		}
+		return () => clearInterval(clockInterval);
+	}, [isActive, timer]);
+
 	return (
 		<div className="timer-container">
 			<div className="clock-container">Timer: {timer}</div>
 			{/* use string interpolation/bracket notation to put time on the page */}
 			{/* setTimer(useInterval)? */}
 			<div className="buttons-container">
-				<button className="timer-button" onClick={pomTimer}>
-					<i className="fa-solid fa-play" />
+				{/* <button
+					className={`button button-primary button-primary-${
+						isActive ? 'active' : 'inactive'
+					}`}
+					onClick={toggle}>
+					{isActive ? 'Pause' : 'Start'}
+				</button> */}
+				<button className="timer-button" onClick={toggleTimer}>
+					{isActive ? (
+						<i className="fa-solid fa-play" />
+					) : (
+						<i className="fa-solid fa-pause"></i>
+					)}
 				</button>
-				<button className="timer-button">
-					<i className="fa-solid fa-pause"></i>
-				</button>
+
 				<button className="timer-button">
 					<i className="fa-solid fa-rotate-right"></i>
 				</button>
